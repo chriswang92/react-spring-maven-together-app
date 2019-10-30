4301605d16391771585d65b306d9ded868d48e07
bvschriswang <chriswang.bvs@gmail.com>
2019-10-30 16:02:12 -0400

    update server.js

--- react-frontend/server.js
+++ react-frontend/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 const history = require('connect-history-api-fallback');
+app.use(history());
 // const connect = require('connect');
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -16,6 +17,5 @@ app.get('/ping', function(req, res) {
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.use(history());
 
 app.listen(port);
