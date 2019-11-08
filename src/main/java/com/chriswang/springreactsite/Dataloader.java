97642ff85915b54adb53ef0dccaf93d338c1225b
bvschriswang <chriswang.bvs@gmail.com>
2019-11-08 15:58:15 -0500

    fix

--- src/main/java/com/chriswang/springreactsite/Dataloader.java
+++ src/main/java/com/chriswang/springreactsite/Dataloader.java
@@ -1,29 +1,29 @@
 package com.chriswang.springreactsite;
 
-//import com.chriswang.springreactsite.model.Book;
-//import com.chriswang.springreactsite.model.User;
-//import com.chriswang.springreactsite.repository.BookRepository;
-//import com.chriswang.springreactsite.repository.UserRepository;
-//import org.apache.commons.lang3.RandomStringUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class Dataloader implements ApplicationRunner {
-//
-//    @Autowired
-//    private BookRepository bookRepository;
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Override
-//    public void run(ApplicationArguments args) throws Exception {
-//        for (int i=0;i<5;i++) {
-//            bookRepository.save(new Book("AutoLoadedBook - "+ RandomStringUtils.randomAlphabetic(10)));
-//        }
-//        for (int ii=0;ii<5;ii++) {
-//            userRepository.save(new User("AutoLoadedUser - "+ RandomStringUtils.randomAlphabetic(10)));
-//        }
-//    }
-//}
+import com.chriswang.springreactsite.model.Book;
+import com.chriswang.springreactsite.model.User;
+import com.chriswang.springreactsite.repository.BookRepository;
+import com.chriswang.springreactsite.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dataloader implements ApplicationRunner {
+
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        for (int i=0;i<5;i++) {
+            bookRepository.save(new Book("AutoLoadedBook - "+ RandomStringUtils.randomAlphabetic(10)));
+        }
+        for (int ii=0;ii<5;ii++) {
+            userRepository.save(new User("AutoLoadedUser - "+ RandomStringUtils.randomAlphabetic(10)));
+        }
+    }
+}
