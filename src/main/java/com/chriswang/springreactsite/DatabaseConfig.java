b1eef8427f8f299ae296205696f20548a4b3aa68
bvschriswang <chriswang.bvs@gmail.com>
2019-11-08 09:51:34 -0500

    fix

--- src/main/java/com/chriswang/springreactsite/DatabaseConfig.java
+++ src/main/java/com/chriswang/springreactsite/DatabaseConfig.java
@@ -1,19 +1,32 @@
 package com.chriswang.springreactsite;
-//
-//import com.zaxxer.hikari.*;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.*;
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class DatabaseConfig {
-//
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URISyntaxException;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
+    }
+
+
 //    @Value("${spring.datasource.url}")
 //    private String dbUrl;
 //
 //    @Value("${spring.datasource.driverClassName")
 //    private String driverClassName;
-//
 //    @Bean
 //    public DataSource dataSource() {
 //        HikariConfig config = new HikariConfig();
@@ -23,4 +36,4 @@ package com.chriswang.springreactsite;
 //        config.setPassword("a$920705");
 //        return new HikariDataSource(config);
 //    }
-//}
+}
