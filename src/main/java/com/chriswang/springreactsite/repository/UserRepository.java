fa43309c7dfdf2856c2a52f08d6ced2108351b89
bvschriswang <chriswang.bvs@gmail.com>
2019-11-08 14:51:21 -0500

    fix

--- src/main/java/com/chriswang/springreactsite/repository/UserRepository.java
+++ src/main/java/com/chriswang/springreactsite/repository/UserRepository.java
@@ -2,11 +2,8 @@ package com.chriswang.springreactsite.repository;
 
 import com.chriswang.springreactsite.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-/**
- * @author CWang
- */
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
